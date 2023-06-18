9291b598c0eefc5fe87abaf50f44f356b01038dd
Furkan <furkangonulal.dev@gmail.com>
2023-06-19 00:45:48 +0300

    ....ya

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -70,7 +70,11 @@ app.delete('/todos/:id', (req, res) => {
   }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// Start the server only if not in a testing environment
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
